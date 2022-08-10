49857eabba92b69ad67d9df342d9663d1ffc8ce5
Connor <connor.cs@protonmail.com>
2022-08-10 14:38:53 -0400

    completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,22 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  # get "/http://localhost:9292/todos" do
-    
-  # end
+  get "/http://localhost:9292/todos" do
+    tasks = Task.all
+    tasks.to_json
+  end
 
-  post "/todos" do
+  post "/http://localhost:9292/todos" do
     task = Task.create(
-
+    taskname: params[:taskname]
+    category_id: params[:category_id]
     )
   end
 
+  delete "/http://localhost:9292/todos/:id" do
+    task = task.find(params[id])
+    task.destroy
+    task.to_json
+  end
+  
 end
